60f63b00ee54e312b384ad03c4b589471d398948
Felicia <nemet.felicia@gmail.com>
2021-12-27 13:20:06 -0500

    added more pseudo

--- app.js
+++ app.js
@@ -36,6 +36,29 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
      });
 
 
+// Psuedo code
+// const fetchPokemon = () => {
+
+
+     // const promises = [];
+     // for (let i = 1; i <= 150; i++) {
+
+          // const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+          // promises.push(fetch(url).then((res) => res.json()));
+     // }
+
+     // Promise.all(promises).then(results => {
+          // const pokemon = results.map((data) => ({
+               // name: data.name,
+               // id: data.id,
+               // image: data.sprites['front_default'],
+               // type: data.types.map((type) => type.type.name).join(', ')
+          // }));
+          // displayPokemon(pokemon);
+
+     // });
+// };
+
 
 
 
