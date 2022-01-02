2d2d8f9f755a42b392f8f437ffe78fe0e113ab12
Evans <evanske2@gmail.com>
2022-01-02 16:32:34 -0500

    accessing pokemon api

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const bulbasaur = fetch('https://pokeapi.co/api/v2/pokemon/')
-
 // url for Bulbasaur : (https://pokeapi.co/api/v2/pokemon/1/)
 
-
 // url for Charmander : (https://pokeapi.co/api/v2/pokemon/4/)
-
 
 // url for Squirtle : (https://pokeapi.co/api/v2/pokemon/7/)
 
@@ -12,29 +8,66 @@ const bulbasaur = fetch('https://pokeapi.co/api/v2/pokemon/')
 
 
 
-// fetch('https://pokeapi.co/api/v2/pokemon/')
-     // .then(function (response) {
-          // return response.json();
-     // })
-     // .then(function (jsonResult) {
-          // console.log(jsonResult);
+// create namespace for the app:
+const pokemonStarterApp = {};
 
-          // const myPokemon = jsonResult;
-          // console.log(myPokemon.results[0].name);
-     // });
+// create init function
+pokemonStarterApp.init = function () {
+     pokemonStarterApp.getPokemon();
+};
+
+// store the api URL as a property on the app
+pokemonStarterApp.url = 'https://pokeapi.co/api/v2/pokemon/?limit=30';
 
 
+pokemonStarterApp.getPokemon = function() {
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-     .then(function (response) {
-          return response.json();
+     const url = new URL(pokemonStarterApp.url);
+
+     fetch(url)
+          .then(function(response) {
+               return response.json();
+          })
+          .then(function(jsonResult) {
+
+               const myPokemon = jsonResult;
+
+               console.log(myPokemon.results);
+
+               const bulbasaurObj = myPokemon.results[0];
+               const charmanderObj = myPokemon.results[3];
+               const squirtleObj = myPokemon.results[6];
+               const pikachuObj = myPokemon.results[24];
+
+               console.log(bulbasaurObj);
+               console.log(charmanderObj);
+               console.log(squirtleObj);
+               console.log(pikachuObj);
+
+               // pokemonStarterApp.displayPokemon(jsonResult);
+          });
+
+}
+
+// display the pokemon on the page
+pokemonStarterApp.displayPokemon = function(jsonData) {
+     const ulElement = document.querySelector('ul');
+     jsonData.forEach((imageItem) => {
+          // create new li elements for each pokemon
+          const liElement = document.createElement('li');
+          const imageElement = document.createElement('img');
+
+          imageElement.src = imageItem.urls.regular;
+          imageElement.alt = jsonData.alt_description;
+
+          ulElement.append(liElement);
+          liElement.appendChild(imageElement);
      })
-     .then(function (jsonResult) {
-          console.log(jsonResult);
+};
 
-          const myPokemon = jsonResult;
-          console.log(myPokemon.results[0].name);
-     });
+// initialize the app
+pokemonStarterApp.init();
+
 
 
 
