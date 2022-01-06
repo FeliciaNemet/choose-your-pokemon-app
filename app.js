e18f5d73355706568e92b96a60d1c12e502aff27
Felicia <nemet.felicia@gmail.com>
2022-01-06 15:01:41 -0500

    tried to repair the issues

--- app.js
+++ app.js
@@ -8,110 +8,63 @@ const pokemonStarterApp = {};
 
 // create init function
 pokemonStarterApp.init = () => {
-     // pokemonStarterApp.getPokemon();
      pokemonStarterApp.setUpEventListeners();
-
+     pokemonStarterApp.getPokemon();
 };
 
 pokemonStarterApp.setUpEventListeners = () => {
      document.querySelector('#pokemonChoice').addEventListener('change', function () {
-
-     
           pokemonStarterApp.findPokemon = this.value;
 
           console.log(pokemonStarterApp.findPokemon);  // this is displaying the value (name of pokemon)
-     });
-pokemonStarterApp.setUpEventListeners = () => {
-     document.querySelector('#pokemonChoice').addEventListener('change', function() {
-          pokemonStarterApp.findPokemon = this.value;
-          console.log(pokemonStarterApp.findPokemon);
+
           pokemonStarterApp.getPokemon(pokemonStarterApp.findPokemon);
 
      });
-};
+}
+
 
 pokemonStarterApp.getPokemon = (id) => {
      // store the api URL as a property on the app
      const url = `https://pokeapi.co/api/v2/pokemon/${id}?limit=30`;
-     url.search = new URLSearchParams({
-          q: 'query'
-     });
+     // url.search = new URLSearchParams({
+     //      q: 'query'
+     // });
 
 
      fetch(url)
-          .then( (response) => {
+          .then((response) => {
                return response.json();
           })
-          .then( (jsonResult) => {
-
-          console.log(jsonResult);
+          .then((jsonResult) => {
                const myPokemon = jsonResult;
                console.log(myPokemon);
 
-               // // console.log(myPokemon.results);
-               pokemonStarterApp.displayPokemon(jsonResult.pokeObject);
+               // need to call the display function in here
+               // eg: artApp.displayPieces(data.artObjects);
+
+               pokemonStarterApp.displayPokers(myPokemon);
                
           });
 }
 
 // display the pokemon on the page
-pokemonStarterApp.displayPokemon = (pokeDetails) => {
-     const pokeBox = document.querySelector(`#pokeBox`);
-     pokeBox.innerHTML = ``;
+pokemonStarterApp.displayPokemon = (pokeObject) => {
 
-          pokeDetails.forEach( (pokeObject) => {
-               const pokeCube = document.createElement(`div`);
-               pokeCube.classList.add(`pokeContainer`)
+               const pokeName = document.querySelector(`#poke-name`);
+               pokeName.innerHTML = pokemonStarterApp.findPokemon;
 
-               const pokeName = document.createElement(`h3`);
-               pokeName.innerText = jsonResult.pokeName;
+               const pokeNumber = document.querySelector(`#poke-type`);
+               pokeNumber.innerHTML = pokeObject.id;
 
-               const pokeType = document.createElement(`p`);
-               pokeType.innerText = jsonResult.types;
+               const pokeFrontImage = document.querySelector(`#poke-front-image`);
+               pokeFrontImage.src = pokeObject[`sprites`][`front_default`];
 
-               const pokeImage = document.createElement(`img`);
-               pokeImage.src = pokeObject[`sprites`][`front_default`];
-               console.log(pokeObject[`sprites`][`front_default`]);
-               pokeImage.alt = pokeObject.name;
+               const pokeBackImage = document.querySelector(`#poke-back-image`);
+               pokeBackImage.src = pokeObject[`sprites`][`back_default`];
 
-               pokeCube.appendChild(pokeName);
-               pokeCube.appendChild(pokeType);
-               pokeImage.appendChild(pokeImage);
-
-               pokeBox.appendChild(pokeCube);
-
-          });
 };
-
-     // jsonData.forEach((imageItem) => {
-     //      // create new li elements for each pokemon
-     //      const liElement = document.createElement('li');
-          
-     //      const imageElement = document.createElement('img');
-     //      imageElement.src = imageItem.sprites[`front_default`].url;
-     //      imageElement.alt = imageItem.name;
-
-          
-     // })
-
-
-
 
 // initialize the app
 pokemonStarterApp.init();
 
-     const pokeNumber = document.querySelector(`#poke-type`);
-     pokeNumber.innerHTML = pokeObject.id;
-
-     const pokeFrontImage = document.querySelector(`#poke-front-image`);
-     pokeFrontImage.src = pokeObject[`sprites`][`front_default`];
-
-     
-
-     
-               // name: data.name,
-               // id: data.id,
-               // image: data.sprites['front_default']
-
-
-};
