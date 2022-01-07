0a99d8c0820cbf77b5b84e4e7438c817d94e73d0
Evans <evanske2@gmail.com>
2022-01-06 20:34:59 -0500

    created the functions to change the styles

--- app.js
+++ app.js
@@ -8,60 +8,99 @@ const pokemonStarterApp = {};
 
 // create init function
 pokemonStarterApp.init = () => {
-     pokemonStarterApp.setUpEventListeners();
-     pokemonStarterApp.getPokemon();
+   pokemonStarterApp.setUpPokeEventListeners();
+   pokemonStarterApp.getPokemon();
 };
 
-pokemonStarterApp.setUpEventListeners = () => {
-     document.querySelector('#pokemonChoice').addEventListener('change', function () {
-          pokemonStarterApp.findPokemon = this.value;
+pokemonStarterApp.setUpPokeEventListeners = () => {
+   document.querySelector('#pokemonChoice').addEventListener('change', function () {
+      pokemonStarterApp.findPokemon = this.value;
 
-          console.log(pokemonStarterApp.findPokemon);  // this is displaying the value (name of pokemon)
+      // console.log(pokemonStarterApp.findPokemon);  
+      // ^ this is displaying the value (name of pokemon)
 
-          pokemonStarterApp.getPokemon(pokemonStarterApp.findPokemon);
-
-     });
+      pokemonStarterApp.getPokemon(pokemonStarterApp.findPokemon);
+   });
 }
 
 pokemonStarterApp.getPokemon = (id) => {
-     // store the api URL as a property on the app
-     const url = `https://pokeapi.co/api/v2/pokemon/${id}?limit=30`;
-     // url.search = new URLSearchParams({
-     //      q: 'query'
-     // });
+   if (!id) return;
+   // store the api URL as a property on the app
+   const url = `https://pokeapi.co/api/v2/pokemon/${id}?limit=30`;
+   // url.search = new URLSearchParams({
+   //      q: 'query'
+   // });
 
-     fetch(url)
-          .then((response) => {
-               return response.json();
-          })
-          .then((jsonResult) => {
-               const myPokemon = jsonResult;
-               console.log(myPokemon);
+   fetch(url)
+      .then((response) => {
+         return response.json();
+      })
+      .then((jsonResult) => {
+         const myPokemon = jsonResult;
+         console.log(myPokemon);
 
-               // need to call the display function in here
-               // eg: artApp.displayPieces(data.artObjects);
+         // need to call the display function in here
+         // eg: artApp.displayPieces(data.artObjects);
 
-               pokemonStarterApp.displayPokers(myPokemon);
-
-
-          });
+         pokemonStarterApp.displayPokers(myPokemon);
+         // pokemonStarterApp.removePokeClasses(id);
+         pokemonStarterApp.addPokeClasses(id);
+      });
 }
-
 
 pokemonStarterApp.displayPokers = (pokeObject) => {
 
-     const pokeName = document.querySelector(`#poke-name`);
-     pokeName.innerHTML = pokemonStarterApp.findPokemon;
+   const pokeName = document.querySelector(`#poke-name`);
+   pokeName.innerHTML = pokemonStarterApp.findPokemon;
 
-     const pokeNumber = document.querySelector(`#poke-type`);
-     pokeNumber.innerHTML = pokeObject.id;
+   const pokeNumber = document.querySelector(`#poke-number`);
+   pokeNumber.innerHTML = pokeObject.id;
 
-     const pokeFrontImage = document.querySelector(`#poke-front-image`);
-     pokeFrontImage.src = pokeObject[`sprites`][`front_default`];
+   const pokeType = document.querySelector(`#poke-type`);
+   pokeType.innerHTML = pokeObject.types[0].type.name;
 
-     const pokeBackImage = document.querySelector(`#poke-back-image`);
-     pokeBackImage.src = pokeObject[`sprites`][`back_default`];
+   const pokeFrontImage = document.querySelector(`#poke-front-image`);
+   pokeFrontImage.src = pokeObject[`sprites`][`front_default`];
 
+   const pokeBackImage = document.querySelector(`#poke-back-image`);
+   pokeBackImage.src = pokeObject[`sprites`][`back_default`];
+
+}
+
+pokemonStarterApp.addPokeClasses = (pokemonName) => {
+   const pokeName = document.querySelector(`#poke-name`);
+
+   // add squirtle styles
+   if (pokemonName === 'squirtle') {
+      pokeName.classList.add(`squirtle-styles`);
+      pokeName.classList.remove(`charmander-styles`);
+      pokeName.classList.remove(`pikachu-styles`);
+      pokeName.classList.remove(`bulbasaur-styles`);
+   };
+
+   // add bulbasaur styles
+   if (pokemonName === 'bulbasaur') {
+      pokeName.classList.add(`bulbasaur-styles`);
+      pokeName.classList.remove(`charmander-styles`);
+      pokeName.classList.remove(`pikachu-styles`);
+      pokeName.classList.remove(`squirtle-styles`);
+   };
+
+   // add pikachu styles
+   if (pokemonName === 'pikachu') {
+      pokeName.classList.add(`pikachu-styles`);
+      pokeName.classList.remove(`charmander-styles`);
+      pokeName.classList.remove(`squirtle-styles`);
+      pokeName.classList.remove(`bulbasaur-styles`);
+   };
+
+   // add charmander styles
+   if (pokemonName === 'charmander') {
+      pokeName.classList.add(`charmander-styles`);
+      pokeName.classList.remove(`squirtle-styles`);
+      pokeName.classList.remove(`pikachu-styles`);
+      pokeName.classList.remove(`bulbasaur-styles`);
+   };
 
 }
 
