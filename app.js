13de978fe90a70dda4f13651a86951cedddb6b1d
Evans <evanske2@gmail.com>
2022-01-08 14:06:41 -0500

    toggle shiny button edits

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-// url for Bulbasaur : (https://pokeapi.co/api/v2/pokemon/1/)
-// url for Charmander : (https://pokeapi.co/api/v2/pokemon/4/)
-// url for Squirtle : (https://pokeapi.co/api/v2/pokemon/7/)
-// url for Pikachu : (https://pokeapi.co/api/v2/pokemon/25/)
-
 // create namespace for the app:
 const pokemonStarterApp = {};
 
 // create init function
 pokemonStarterApp.init = () => {
-     pokemonStarterApp.setUpPokeEventListeners();
-     pokemonStarterApp.getPokemon();
-     
+   pokemonStarterApp.setUpPokeEventListeners();
+   pokemonStarterApp.getPokemon();
 };
 
 pokemonStarterApp.setUpPokeEventListeners = () => {
@@ -19,44 +13,38 @@ pokemonStarterApp.setUpPokeEventListeners = () => {
 
       // console.log(pokemonStarterApp.findPokemon);  
       // ^ this is displaying the value (name of pokemon)
-          
-          pokemonStarterApp.getPokemon(pokemonStarterApp.findPokemon);
-          
-     });
-}
 
-let count = 0
+      pokemonStarterApp.getPokemon(pokemonStarterApp.findPokemon);
+
+   });
+};
 
 pokemonStarterApp.makeShinyEventListeners = (pokeObject) => {
-
-
-let count = 0
-
-pokemonStarterApp.makeShinyEventListeners = (pokeObject) => {
+   
+   let count = 0
 
    document.querySelector(`#shiny`).addEventListener(`click`, function () {
-         
-         if (count === 0) {
-            const pokeFrontImage = document.querySelector(`#poke-front-image`);
-            pokeFrontImage.src = pokeObject[`sprites`][`front_shiny`];
 
-            const pokeBackImage = document.querySelector(`#poke-back-image`);
-            pokeBackImage.src = pokeObject[`sprites`][`back_shiny`];
+      if (count === 0) {
+         const pokeFrontImage = document.querySelector(`#poke-front-image`);
+         pokeFrontImage.src = pokeObject[`sprites`][`front_shiny`];
 
-            count = 1;
-         
-         } else if (count === 1) {
-            const pokeFrontImage = document.querySelector(`#poke-front-image`);
-            pokeFrontImage.src = pokeObject[`sprites`][`front_default`];
-               
-            const pokeBackImage = document.querySelector(`#poke-back-image`);
-               pokeBackImage.src = pokeObject[`sprites`][`back_default`];
-               
-               count = 0;
-         } 
+         const pokeBackImage = document.querySelector(`#poke-back-image`);
+         pokeBackImage.src = pokeObject[`sprites`][`back_shiny`];
 
+         count = 1;
+
+      } else if (count === 1) {
+         const pokeFrontImage = document.querySelector(`#poke-front-image`);
+         pokeFrontImage.src = pokeObject[`sprites`][`front_default`];
+
+         const pokeBackImage = document.querySelector(`#poke-back-image`);
+         pokeBackImage.src = pokeObject[`sprites`][`back_default`];
+
+         count = 0;
+      }
    })
-}
+};
 
 pokemonStarterApp.getPokemon = (id) => {
    if (!id) return;
@@ -68,19 +56,17 @@ pokemonStarterApp.getPokemon = (id) => {
 
    fetch(url)
       .then((response) => {
-      return response.json();
+         return response.json();
       })
       .then((jsonResult) => {
-      const myPokemon = jsonResult;
-      console.log(myPokemon);
+         const myPokemon = jsonResult;
+         console.log(myPokemon);
 
-      pokemonStarterApp.displayPokers(myPokemon);
-      // pokemonStarterApp.removePokeClasses(id);
-
-      pokemonStarterApp.makeShinyEventListeners(myPokemon);
-      pokemonStarterApp.addPokeClasses(id);
+         pokemonStarterApp.displayPokers(myPokemon);
+         pokemonStarterApp.makeShinyEventListeners(myPokemon);
+         pokemonStarterApp.addPokeClasses(id);
       });
-}
+};
 
 pokemonStarterApp.displayPokers = (pokeObject) => {
 
@@ -100,46 +86,36 @@ pokemonStarterApp.displayPokers = (pokeObject) => {
    pokeBackImage.src = pokeObject[`sprites`][`back_default`];
 
    const shiny = document.querySelector(`#shiny`);
-   shiny.innerHTML = ` toggle shiny ${pokemonStarterApp.findPokemon}`;
-
-}
+   shiny.innerHTML = `toggle shiny ${pokemonStarterApp.findPokemon}`;
+};
 
 pokemonStarterApp.addPokeClasses = (pokemonName) => {
    const pokeName = document.querySelector(`#poke-name`);
 
    // add squirtle styles
    if (pokemonName === 'squirtle') {
+      pokeName.removeAttribute("class");
       pokeName.classList.add(`squirtle-styles`);
-      pokeName.classList.remove(`charmander-styles`);
-      pokeName.classList.remove(`pikachu-styles`);
-      pokeName.classList.remove(`bulbasaur-styles`);
    };
 
    // add bulbasaur styles
    if (pokemonName === 'bulbasaur') {
+      pokeName.removeAttribute("class");
       pokeName.classList.add(`bulbasaur-styles`);
-      pokeName.classList.remove(`charmander-styles`);
-      pokeName.classList.remove(`pikachu-styles`);
-      pokeName.classList.remove(`squirtle-styles`);
    };
 
    // add pikachu styles
    if (pokemonName === 'pikachu') {
+      pokeName.removeAttribute("class");
       pokeName.classList.add(`pikachu-styles`);
-      pokeName.classList.remove(`charmander-styles`);
-      pokeName.classList.remove(`squirtle-styles`);
-      pokeName.classList.remove(`bulbasaur-styles`);
    };
 
    // add charmander styles
    if (pokemonName === 'charmander') {
+      pokeName.removeAttribute("class");
       pokeName.classList.add(`charmander-styles`);
-      pokeName.classList.remove(`squirtle-styles`);
-      pokeName.classList.remove(`pikachu-styles`);
-      pokeName.classList.remove(`bulbasaur-styles`);
    };
-
-}
+};
 
 // initialize the app
 pokemonStarterApp.init();
